6407f3a1ce4c0971dfff8a18cae7f9731f0907c8
Fernanda Ribeiro <fer.maiafr@gmail.com>
2021-11-04 16:06:02 -0300

    feat: Add kafka producer/consumer assignment

--- kafka/order-consumer/src/main/java/io/github/fernanda/maia/OrderConsumer.java
+++ kafka/order-consumer/src/main/java/io/github/fernanda/maia/OrderConsumer.java
@@ -17,7 +17,7 @@ public class OrderConsumer {
         // Producer >> serializer | Consumer >> deserializer
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-        
+
         props.setProperty("group.id", "OrderGroup");
 
         KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);

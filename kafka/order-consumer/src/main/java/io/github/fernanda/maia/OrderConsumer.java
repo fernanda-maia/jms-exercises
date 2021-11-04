61ab6e7bf6ba66f855872d036e07eb0ff58da3ef
Fernanda Ribeiro <fer.maiafr@gmail.com>
2021-11-04 15:30:14 -0300

    feat: Add kafka consumer example

---
+++ kafka/order-consumer/src/main/java/io/github/fernanda/maia/OrderConsumer.java
@@ -0,0 +1,36 @@
+package io.github.fernanda.maia;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class OrderConsumer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+
+        props.setProperty("bootstrap.servers", "localhost:9092");
+
+        // Producer >> serializer | Consumer >> deserializer
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+        
+        props.setProperty("group.id", "OrderGroup");
+
+        KafkaConsumer<String, Integer> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("OrderTopic"));
+
+        ConsumerRecords<String, Integer> records = consumer.poll(Duration.ofSeconds(20));
+
+        records.forEach(order -> {
+            System.out.println("Product Name: " + order.key());
+            System.out.println("Quantity: " + order.value());
+        });
+
+        consumer.close();
+
+    }
+}
